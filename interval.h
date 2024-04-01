ccbcfa50327de5937fb97ebd6e8f8af65a8828ef
azmine-1 <sachchha222@gmail.com>
2024-04-01 16:10:07 -0400

    added more classes and final imageoutput

---
+++ interval.h
@@ -0,0 +1,36 @@
+#ifndef INTERVAL_H
+#define INTERVAL_H
+class interval {
+  public:
+    double min, max;
+    interval() : min(+infinity), max(-infinity) {} 
+
+    interval(double _min, double _max) : min(_min), max(_max) {}
+
+    double size() const {
+        return max - min;
+    }
+
+    interval expand(double delta) const {
+        auto padding = delta/2;
+        return interval(min - padding, max + padding);
+    }
+    bool contains(double x) const {
+        return min <= x && x <= max;
+    }
+    bool surrounds(double x) const {
+        return min < x && x < max;
+    }
+
+    double clamp(double x) const {
+        if (x < min) return min;
+        if (x > max) return max;
+        return x;
+    }
+
+    static const interval empty, universe;
+};
+
+const interval interval::empty    = interval(+infinity, -infinity);
+const interval interval::universe = interval(-infinity, +infinity);
+#endif
